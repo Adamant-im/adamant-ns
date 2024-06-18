c23effd7d8294fd317e0d3e5d73903867870b3a4
bludnic <victorcpla@gmail.com>
2024-06-18 21:23:33 +0100

    chore: configure ESLint, Prettier and TypeScript

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config({
+  files: ['**/*.ts'],
+  plugins: {
+    '@typescript-eslint': tseslint.plugin
+  },
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      project: true
+      // sourceType: 'module'
+    }
+  },
+  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+  rules: {
+    '@typescript-eslint/no-unsafe-argument': 'error',
+    '@typescript-eslint/no-unsafe-assignment': 'error',
+    '@typescript-eslint/no-unsafe-call': 'error',
+    '@typescript-eslint/no-unsafe-member-access': 'error',
+    '@typescript-eslint/no-unsafe-return': 'error'
+  }
+})
---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import('prettier').Config}
+ */
+const config = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true
+}
+
+export default config
