3a40b09902f419f29f6e11e4b6017a524e8e4267
bludnic <victorcpla@gmail.com>
2024-06-18 22:31:20 +0100

    chore(ESLint): lint only `src` dir

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config({
-  files: ['**/*.ts'],
+  files: ['src/**/*.ts'],
   plugins: {
     '@typescript-eslint': tseslint.plugin
   },
