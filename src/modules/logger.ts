9bc55711914f68455361413d77ba8eab1cf3a281
bludnic <victorcpla@gmail.com>
2024-12-07 14:39:48 +0000

    refactor: simplify logger

--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -1,11 +1,10 @@
 import path from 'path'
-import { createLogger as createPinoLogger } from 'adamant-module-logger'
+import { createLogger } from 'adamant-module-logger'
+
 import { config } from '../config/index.js'
 
-export const { logger, middlewareLogger } = createPinoLogger(
-  {
-    name: config.app.name
-  },
+export const { logger } = createLogger(
+  { name: config.app.name },
   {
     destination: path.join(config.app.projectRoot, 'logs')
   }
